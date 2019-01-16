124be60d963e287e247533df0104d04e37f4c1ef
DavideBusacca <thebabytranslator@gmail.com>
2019-01-16 10:37:14 +0100

    Updating utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import sys, os, copy
+import errno
 import subprocess
 import numpy as np
 from scipy.signal import hanning
@@ -19,24 +20,24 @@ class NewModule():
     def __init__(self):
         self.clear()
         self.update()
-        
+
     def getParam(self):
         pass
-        
+
     def setParam(self):
         pass
-        
+
     def update(self):
         pass
 
     def process(self):
         pass
-        
+
     def clockProcess(self):
         pass
-    
+
     def clear(self):
-        pass    
+        pass
 
 def distance2signals(x, y, frameSize):
     clean = True
@@ -57,18 +58,18 @@ def myOverlapAdd(xw, windowSize, hopSize):
         x[ch:ch+windowSize]=x[ch:ch+windowSize]+xw[cw:cw+windowSize]
         ch=ch+hopSize #current hop
         cw=cw+windowSize #current window
-    
+
     return x
 
 def windowing(x, frameSize, typeWindow='hann'):
     if typeWindow == 'hann':
         return x*hanning(frameSize)
-        
+
 def zeroPhasing(x, frameSize, zeroPadding, fftshift=True, inverse=False):
     '''
     fftShift
     N is frameSize+zeroPadding
-    '''      
+    '''
     if inverse == False:
         if fftshift == True:
             N = frameSize+zeroPadding
@@ -79,39 +80,39 @@ def zeroPhasing(x, frameSize, zeroPadding, fftshift=True, inverse=False):
             fftbuffer[-h2:] = x[:h2]
         else:
             fftbuffer = np.append(x, np.zeros(zeroPadding))
-    if inverse == True:        
+    if inverse == True:
         N = frameSize+zeroPadding
         h1 = int(np.floor((x.size+1)/2))
         h2 = int(np.floor(x.size/2))
         fftbuffer = np.zeros(N)
         fftbuffer[:h1] = x[h2:]
         fftbuffer[-h2:] = x[:h2]
-        if zeroPadding != 0:  
+        if zeroPadding != 0:
             #deleting zeros
             z1 = int(np.floor((zeroPadding+1)/2))
             z2 = int(np.floor(zeroPadding)/2)
             fftbuffer = fftbuffer[z1:-z2] #attention, when z2 is 0 the array is empty(solved using the if)
-    
-    return fftbuffer  
-    
+
+    return fftbuffer
+
 def amp2db(amp):
-    return 20*np.log10(amp)
+    return 20*np.log10(amp+sys.float_info.epsilon)
 
 def db2amp(db):
     return 10**(db/20)
-    
+
 def getMagnitude(X):
     return np.abs(X)
-    
+
 def getPhase(X):
     #add phase wrapping!
     return np.angle(X)
-    
+
 def getSpectrogramAxis(X, fs, hopSize):
-    t = np.arange(X.shape[0])*hopSize/float(fs)  
-    f = np.arange(X.shape[1]/2+1)*float(fs/2)/float(X.shape[1]/2+1) 
-    return t, f  
-    
+    t = np.arange(X.shape[0])*hopSize/float(fs)
+    f = np.arange(X.shape[1]/2+1)*float(fs/2)/float(X.shape[1]/2+1)
+    return t, f
+
 def getTimeAxis(x, fs):
     return np.arange(len(x))/float(fs)
 
@@ -138,8 +139,8 @@ def wavplay(filename):
 			else:
 				print("Cannot play sound, winsound could not be imported")
 		else:
-			print("Platform not recognized")    
-			
+			print("Platform not recognized")
+
 def wavread(filename):
     #Adapted from sms-tools
 
@@ -154,9 +155,9 @@ def wavread(filename):
 
 	fs, x = read(filename)
 	x = np.float32(x)/norm_fact[x.dtype.name]
-	
+
 	return x, fs
-	
+
 def wavwrite(y, fs, filename):
     #Adapted from sms-tools
 
@@ -175,10 +176,10 @@ def peakFollower(x, fs, releaseTime):
     release_factor = np.exp(-1/(releaseTime * fs))
     x = abs(x)
     envelope = np.zeros_like(x)
-    for id in range(np.size(x)-1): 
+    for id in range(np.size(x)-1):
         i=id+1
         envelope[i] = max(x[i], envelope[i-1]*release_factor )
-        
+
     return envelope
 
 def resample(x, sr_old, sr_new):
@@ -188,3 +189,14 @@ def resample(x, sr_old, sr_new):
 
 def rms(x):
     return np.sqrt((np.mean(np.power(x, 2))))
+
+def getNSamplesFromDuration(duration, fs, unit=1000):
+#unit = 1000 is when the duration is given in milliseconds
+    return int(np.floor(duration/unit * fs))
+
+def createFolder(directory):
+    try:
+        os.makedirs(directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
