0daeed293f0455e8a2da4c2f020e4982e528c12c
DavideBusacca <busaccadavide@gmail.com>
2019-02-28 21:57:58 +0100

    HPSS is now a class

--- HPSS/HPSS.py
+++ HPSS/HPSS.py
@@ -22,84 +22,202 @@ import matplotlib.pyplot as plt
 from scipy.ndimage import median_filter
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../spectrogram/'))
-import STFT as STFT
-import ISTFT as ISTFT
+import STFT
+import ISTFT
 
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../'))
 import utils as U
 
-def hardMask(harm, perc):
-    '''
-    Hard masking for Harmonic/Percussive Source Separation
-    '''
-    mask_harm = harm > perc
-    mask_perc = perc > harm
+class Param_HPSS(U.NewParam):
+    #Class that contains the parameters to create a STFT module
+    def __init__(self,  param_STFT=None, win_harm=31, win_perc=31, masking='hard'):
+        self.param_STFT = param_STFT
+        self.win_harm = win_harm
+        self.win_perc = win_perc
+        self.masking = masking
 
-    return mask_harm, mask_perc
-
-
-def computing_hpss_masks(mX, win_harm=31, win_perc=31, masking='hard'):
-    """
-    Masks used by median-filtering harmonic percussive source separation (HPSS).
-
-    Parameters
-    ----------
-    mX: input magnitude spectrogram
-    win_harm: number of bins used for median filtering in frequency direction 
-    win_perc: number of bins used for median filtering in time direction
-    
-    Returns
-    -------
-    harmonic component
-    percussive component
-    
-    """
-
-    #Computing harmonic enhanced spectrogram
-    harm = np.empty_like(mX)
-    harm[:] = median_filter(mX, size=(win_perc, 1), mode='reflect')
-	#Computing percussive enhanced spectrogram
-    perc = np.empty_like(mX)
-    perc[:] = median_filter(mX, size=(1, win_harm), mode='reflect')
-
-    #Computing harmonic and percussive masks
-    if masking == 'hard':
-        mask_harm, mask_perc = hardMask(harm, perc)
-
-    return mask_harm, mask_perc
-    
-def hpss(x, win_harm=31, win_perc=31, masking='hard', hopSize=512, frameSize=2048, zeroPadding=0, windowType='hann', fftshift=True, normalized=False):
+class HPSS(U.NewModule):
     '''
     Median-filtering harmonic percussive source separation (HPSS)
-    
     '''
-    #Computing STFT
-    X = STFT.STFT(x, frameSize=frameSize, hopSize=hopSize, fftshift=fftshift, windowType=windowType, zeroPadding=zeroPadding)
-    
-    #Computing magnitude and phase spectrograms
-    mX = U.getMagnitude(X)
-    pX = U.getPhase(X)
-    
-    #Computing HPSS masks
-    comp_harm, comp_perc = computing_hpss_masks(mX, win_harm=31, win_perc=31, masking='hard')
-    
-    #Computing Harmonic and Percussive components
-    Y_harm = mX*comp_harm*np.exp(1j*pX)
-    Y_perc = mX*comp_perc*np.exp(1j*pX)
-    y_harm, ow = ISTFT.ISTFT(Y_harm, frameSize=frameSize, hopSize=hopSize, fftshift=fftshift, windowType=windowType, zeroPadding=zeroPadding)
-    y_perc, ow = ISTFT.ISTFT(Y_perc, frameSize=frameSize, hopSize=hopSize, fftshift=fftshift, windowType=windowType, zeroPadding=zeroPadding)
-    
-    return y_harm, y_perc
-    
 
-def callback(nameInput='../sounds/piano.wav', prefixOutput='processed/sine_stretched_HPSS', format='.wav', win_harm=31, win_perc=31, masking='hard', hopSize=512, frameSize=2048, zeroPadding=0, windowType='hann', fftshift=True, normalized=False):
+    def __init__(self, param_HPSS=None):
+        self._STFT_ = STFT.STFT()
+        self._ISTFT_ = ISTFT.ISTFT()
+
+        self.setParam(param_HPSS)
+        self.update()
+        self.clear()
+
+    def getParam(self):
+        return Param_HPSS(self._param_STFT_, win_harm=self._win_harm_, win_perc=self._win_perc_, masking=self._masking_)
+
+    def getParamSTFT(self):
+        return self._param_STFT_
+
+    def getWinHarm(self):
+        return self._win_harm_
+
+    def getWinPerc(self):
+        return self._win_perc_
+
+    def getMasking(self):
+        return self._masking_
+
+    def setParam(self, param_HPSS=None):
+        if param_HPSS == None:
+            param_HPSS = Param_HPSS()
+        self.param_HPSS = param_HPSS
+
+    def setFrameSize(self, frameSize):
+        self.param_HPSS.param_STFT.frameSize = frameSize
+        self._needsUpdate_ = True
+
+    def setHopSize(self, hopSize):
+        self.param_HPSS.param_STFT.hopSize = hopSize
+        self._needsUpdate_ = True
+
+    def setFftshift(self, fftshift):
+        self.param_HPSS.param_STFT.fftshift = fftshift
+        self._needsUpdate_ = True
+
+    def setWindowType(self, windowType):
+        self.param_HPSS.param_STFT.windowType = windowType
+        self._needsUpdate_ = True
+
+    def setZeroPadding(self, zeroPadding):
+        self.param_HPSS.param_STFT.zeroPadding = zeroPadding
+        self._needsUpdate_ = True
+
+    def setWin_harm(self, win_harm):
+        self.param_HPSS.win_harm = win_harm
+        self._needsUpdate_ = True
+
+    def setWin_perc(self, win_perc):
+        self.param_HPSS.win_perc = win_perc
+        self._needsUpdate_ = True
+
+    def setMasking(self, masking):
+        self.param_HPSS.masking = masking
+        self._needsUpdate_ = True
+
+    def update(self):
+        self._param_STFT_ = self.param_HPSS.param_STFT
+        self._win_harm_ = self.param_HPSS.win_harm
+        self._win_perc_ = self.param_HPSS.win_perc
+        self._masking_ = self.param_HPSS.masking
+
+        self._STFT_.setParam(self._param_STFT_)
+        self._ISTFT_.setParam(self._param_STFT_)
+        self._STFT_.update()
+        self._ISTFT_.update()
+
+        self._needsUpdate_ = False
+
+    def hardMask(self, harm, perc):
+        '''
+        Hard masking for Harmonic/Percussive Source Separation
+        '''
+        mask_harm = harm > perc
+        mask_perc = perc > harm
+
+        return mask_harm, mask_perc
+
+    # softMask
+
+    def computing_hpss_masks(self, mX):
+        """
+        Masks used by median-filtering harmonic percussive source separation (HPSS).
+
+        Parameters
+        ----------
+        mX: input magnitude spectrogram
+        win_harm: number of bins used for median filtering in frequency direction
+        win_perc: number of bins used for median filtering in time direction
+
+        Returns
+        -------
+        harmonic component
+        percussive component
+
+        """
+
+        #Computing harmonic enhanced spectrogram
+        harm = np.empty_like(mX)
+        harm[:] = median_filter(mX, size=(self._win_perc_, 1), mode='reflect')
+        #Computing percussive enhanced spectrogram
+        perc = np.empty_like(mX)
+        perc[:] = median_filter(mX, size=(1, self._win_harm_), mode='reflect')
+
+        #Computing harmonic and percussive masks
+        if self._masking_ == 'hard':
+            mask_harm, mask_perc = self.hardMask(harm, perc)
+        # Soft masking can be implemented here
+
+        return mask_harm, mask_perc
+
+    '''
+
+    def clockProcess(self, x):
+        #need to create a STFT object inside
+        #need to create a memory where to store the value that will be used to compute the median
+        #maybe it's just better to create an object for the median before
+        X = self.STFT.clockProcess(x)
+
+        #Computing magnitude and phase spectrograms
+        mX = U.getMagnitude(X)
+        pX = U.getPhase(X)
+
+        #Computing HPSS masks
+        comp_harm, comp_perc = self.computing_hpss_masks_clock(mX) # how to do the masking in real-time?
+
+        #Computing Harmonic and Percussive components
+        Y_harm = mX*comp_harm*np.exp(1j*pX)
+        Y_perc = mX*comp_perc*np.exp(1j*pX)
+
+        y_harm = self._ISTFT_.clockProcess(Y_harm)
+        y_perc = self._ISTFT_.clockProcess(Y_perc)
+
+        return y_harm, y_perc
+    '''
+
+    def clear(self):
+        pass
+
+    def process(self, x):
+        #Computing STFT
+        X = self._STFT_.process(x)
+
+        #Computing magnitude and phase spectrograms
+        mX = U.getMagnitude(X)
+        pX = U.getPhase(X)
+
+        #Computing HPSS masks
+        comp_harm, comp_perc = self.computing_hpss_masks(mX)
+
+        #Computing Harmonic and Percussive components
+        Y_harm = mX*comp_harm*np.exp(1j*pX)
+        Y_perc = mX*comp_perc*np.exp(1j*pX)
+
+        y_harm = self._ISTFT_.process(Y_harm)
+        y_perc = self._ISTFT_.process(Y_perc)
+
+        return y_harm, y_perc
+
+
+def callback(nameInput='../sounds/piano.wav', prefixOutput='processed/sine_stretched_HPSS', format='.wav',
+             win_harm=31, win_perc=31, masking='hard', hopSize=512, frameSize=2048, zeroPadding=0,
+             windowType='hann', fftshift=True):
     
     #Loading audio
     x, fs = U.wavread(nameInput)
-    
-    #Harmonic/Percussive Separation   
-    y_harm, y_perc = hpss(x, win_harm=win_harm, win_perc=win_perc, masking=masking, hopSize=hopSize, frameSize=frameSize, zeroPadding=zeroPadding, windowType=windowType, fftshift=fftshift, normalized=normalized)
-    
+
+    param_STFT = STFT.Param_STFT(hopSize=hopSize, frameSize=frameSize, zeroPadding=zeroPadding, windowType=windowType,
+                            fftshift=fftshift)
+    param_HPSS = Param_HPSS(param_STFT=param_STFT, win_harm=win_harm, win_perc=win_perc, masking=masking)
+
+    y_harm, y_perc = HPSS(param_HPSS=param_HPSS).process(x)
+
     #Writing audio
     U.wavwrite(y_harm, fs, prefixOutput+'harm'+format)
     U.wavwrite(y_perc, fs, prefixOutput+'perc'+format)
@@ -122,15 +240,18 @@ def callback(nameInput='../sounds/piano.wav', prefixOutput='processed/sine_stret
     frameSize_analysis = 3071
     zeroPadding_analysis = 1025
     hopSize_analysis = 1024
+
+    param_visualization = STFT.Param_STFT(frameSize=frameSize_analysis, hopSize=hopSize_analysis, fftshift=fftshift,
+                                          windowType=windowType, zeroPadding=zeroPadding_analysis)
     fftshift = True
     windowType ='hann'
-    X = STFT.STFT(x, frameSize=3071, hopSize=hopSize_analysis, fftshift=fftshift, windowType=windowType, zeroPadding=zeroPadding_analysis)
+    X = STFT.STFT(param_STFT=param_visualization).process(x)
     [tx, fx] = U.getSpectrogramAxis(X, fs, hopSize_analysis)
     endX = int(X.shape[1]/2+1)
-    Y_harm = STFT.STFT(y_harm, frameSize=frameSize_analysis, hopSize=hopSize_analysis, fftshift=fftshift, windowType=windowType, zeroPadding=zeroPadding_analysis)
+    Y_harm = STFT.STFT(param_STFT=param_visualization).process(y_harm)
     endY_harm = int(Y_harm.shape[1]/2+1)
     [ty_harm, fy_harm] = U.getSpectrogramAxis(Y_harm, fs, hopSize_analysis)
-    Y_perc = STFT.STFT(y_perc, frameSize=frameSize_analysis, hopSize=hopSize_analysis, fftshift=fftshift, windowType=windowType, zeroPadding=zeroPadding_analysis)
+    Y_perc = STFT.STFT(param_STFT=param_visualization).process(y_perc)
     endY_perc = int(Y_perc.shape[1]/2+1)
     [ty_perc, fy_perc] = U.getSpectrogramAxis(Y_perc, fs, hopSize_analysis)
     
@@ -158,5 +279,4 @@ def callback(nameInput='../sounds/piano.wav', prefixOutput='processed/sine_stret
     plt.show()
 
 if __name__ == "__main__":
-    y_harm, y_perc = callback()
-    
+    callback()
